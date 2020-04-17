5067b3b507c27d964e0876e456471b37d5680b75
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-04-17 23:07:45 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./shared/module/material.module";
+import { CalculatorComponent } from './calculator/calculator.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      CalculatorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        FlexLayoutModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
