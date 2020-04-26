cd62c4494b0e52b3c7232f604e41c26f46960ff1
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-04-26 12:19:37 +0300

    calculator created)

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent{
+  calcNumber: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  value = '';
+  private prevValue = '';
+  private curValue = '';
+  result = '';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  checkVal(value: string) {
+    if (this.result !== '') {
+      this.prevValue = this.curValue;
+      this.curValue = value;
+    }
+    console.log(this.result);
+    if (value === 'AC') {
+      this.result = '';
+    } else if (value === '=') {
+      // tslint:disable-next-line:no-eval
+      this.result = eval(this.result);
+    } else {
+      this.result += value;
+    }
+  }
 }
